82ed84df829b8ddf440462c61e0aa262bd92f23d
Rohit <125853279+rohitmouryar@users.noreply.github.com>
2025-09-10 11:16:53 +0000

    container config done

---
+++ 01_basics/05_dataTypes_summary.js
@@ -0,0 +1,35 @@
+//Primitive data type
+//  7 Types : String , Number, Null, undefined, Symbol, Boolean, Bigint
+
+
+
+// JS statically or dynamically?
+
+const score = 100
+const scoreValue = 100.3
+
+const outsideTem = null;
+const isLoggedIn = false;
+let useEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(is === anotherId)
+
+
+
+//Reference type (Non primitive Data type)
+// Array, Object, Function
+
+
+const  names = ['Rohit', 'ROshan', 'Rajendra']
+let myObj  = {
+    name:'Raunak',
+    age : 23
+}
+
+const myFun = function(){
+    console.log("Hello World");
+} 
