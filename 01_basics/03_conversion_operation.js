724fd2847b728277fc8c01ae9752ab1babc5fcea
Rohit <125853279+rohitmouryar@users.noreply.github.com>
2025-09-10 09:42:03 +0000

    container config done

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,81 @@
+// let score = "33";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let value = undefined
+// console.log(typeof value)
+// console.log(value)
+
+// Conversion
+// "33" = 33
+// "33abc" = NaN
+// true = 1;
+// false = 0
+
+// let num = 1;
+// let bool = Boolean(num)
+// console.log(bool);   // true
+// console.log(typeof bool);   // boolean
+// let num1 = String(num)
+// console.log(typeof num1);   // string
+
+
+
+// let str;
+// let str1 = "Roshan"
+// console.log(str)  // undefined
+// console.log(str1) //roshan
+
+// ****************************************** Operation ****************************************
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// let str1 = "Hello"
+// let str2 = " Rohit"
+// let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log("1" + 2);         //12
+// console.log(1 + "2");        //12
+// console.log("1" + 2 + 2);   //122
+// console.log(1 + "2" + 2);   //122
+// console.log(1 + 2 + "2");   //32
+
+// console.log( (3+4) * (5-3) * 4)     //56
+// console.log(true);      // true
+// console.log(+"");       //0
+// console.log(true+);  // UNEXPECTED 
+
+let gameCounter = 100;
+let post = gameCounter++;
+console.log(post);
+
+let pre = ++gameCounter;
+console.log(pre);
+
+
+
+
+
+
+
+
+
+
+
