feddb603d33497a1efac2691ed3daa824500c576
Rohit <125853279+rohitmouryar@users.noreply.github.com>
2025-09-11 09:16:06 +0000

    Array part 2

---
+++ 02_basics/02_Array.js
@@ -0,0 +1,31 @@
+const boys = ["rohit", "darshan", "Rohan"]
+const girls = ["Seema", "khushboo","Shadhna"]
+
+// boys.push(girls)
+// console.log(boys);
+// console.log(boys[3]);
+
+// const allNames = boys.concat(girls); //old way
+// console.log(allNames);
+
+// const allNames = [...boys,...girls]       // using spread operator for marge 2 array
+// console.log(allNames);
+
+// const anotherArr = [1,2,3, [4, 5, 6],7, [6,7, [4,5]]]
+// const real_another_array = anotherArr.flat(Infinity)
+// console.log(real_another_array);
+
+
+// console.log(Array.isArray("Rohit"))     // false
+// console.log(Array.from("Rohit"))     // Convert string into array
+// console.log(Array.from({name:"Rohit"}))     // CInteresting
+
+
+// Add in the array
+let score1 = 345
+let score2 = 456
+let score3 = 879
+console.log(Array.of(score1,score2,score3));
+ 
+
+
