46b76dfaaa6f77992266cf2c588b982b09fe3ce0
Rohit <125853279+rohitmouryar@users.noreply.github.com>
2025-09-11 07:18:55 +0000

    container config done

--- 01_basics/06_number_and_math.js
+++ 01_basics/06_number_and_math.js
@@ -1,9 +1,61 @@
-const { useDeferredValue } = require("react");
-
-const score = 234
-console.log(score);
+// const score = 234
+// console.log(score);     //234
 
 
-const balance = new Number(234)
-console.log(balance);
+// const balance = new Number(234)
+// console.log(balance);       //[Number: 234]
+
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);  //3
+// console.log(balance.toFixed(1));        //234.0
+
+
+// // ***
+// const othernumber = 23.4567
+// console.log(othernumber.toPrecision());         //23.4567
+// console.log(othernumber.toPrecision(3));        //23.5
+
+
+// const hundred = 1000000
+// console.log(hundred.toLocaleString());      //1,000,000
+// console.log(hundred.toLocaleString('en-IN'));    //10,00,000 in Indian
+
+
+//++++++++++++++++++++++++++++++++++ Math +++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);  //seen in console
+
+// console.log(Math.abs(-4));      //absolute value 4
+
+// console.log(Math.round(4.6));   //5
+
+// console.log(Math.ceil(4.2));    // top value5
+
+// console.log(Math.floor(4.6));   //4 down value
+
+// console.log(Math.min(4, 5, 6, 2));      //minmum value 2
+
+// console.log(Math.max(4, 5, 6, 2));      //maximum value  6
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log((Math.random()*10) +1);   
+
+const min = 10
+const max = 20
+console.log (Math.floor(Math.random()* (max - min + 1)) + min)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
