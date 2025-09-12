b09f4d868d308cc578df92e26f5764781c3e4855
Rohit <125853279+rohitmouryar@users.noreply.github.com>
2025-09-12 12:24:36 +0000

    loops discussion

--- 05_iteration/05_five.js
+++ 05_iteration/05_five.js
@@ -1,4 +1,40 @@
 const coding = ["js", "ruby", "java", "python", "cpp"]
 coding.forEach( function (val){
-    console.log(val);
+    // console.log(val);
 } )
+
+
+
+//arrow functionn
+coding.forEach((item)=> {
+    // console.log(item);    
+})
+
+
+
+//++++++++++++++++++++++
+function printme(item){
+    console.log(item);    
+}
+// coding.forEach(printme)
+
+//++++++++++++++
+coding.forEach((item, index, arr)=>{
+    // console.log(item, index, arr);   
+})
+
+
+//++++++++++++++
+const mycoding = [{
+    languagename : "Js",
+    languagefile: "jsfile"
+},
+{
+    languagename : "java",
+    languagefile : "javafile"
+}
+]
+
+mycoding.forEach( (item) => {
+    console.log(item.languagefile);    
+})
